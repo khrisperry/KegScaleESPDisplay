28b28b2c92f9b02848df731ef45f8521a898f4be
khrisperry <perry.khris@gmail.com>
2026-09-02 16:00:18 -0400

    Use native-resolution fonts: add components/epaper/include/epaper_fonts.h

---
+++ components/epaper/include/epaper_fonts.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    uint32_t bitmap_offset;
+    uint8_t width;
+    uint8_t height;
+    int8_t x_offset;
+    int8_t y_offset;
+    uint8_t x_advance;
+} epaper_glyph_t;
+
+typedef struct {
+    const uint8_t *bitmap;
+    const epaper_glyph_t *glyphs;
+    uint8_t first;
+    uint8_t last;
+    uint8_t line_height;
+} epaper_font_t;
+
+extern const epaper_font_t EPAPER_FONT_HERO_SANS_54;
+extern const epaper_font_t EPAPER_FONT_MONO_16;
+extern const epaper_font_t EPAPER_FONT_MONO_13;
+extern const epaper_font_t EPAPER_FONT_MONO_10;
+
+#ifdef __cplusplus
+}
+#endif
